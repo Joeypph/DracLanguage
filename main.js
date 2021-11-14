46c44a2fa948a1f65fcd6c53b8b505c5bf0ff583
ManuelSalvador27 <manuelsalvador2799@gmail.com>
2021-11-14 10:56:13 -0600

    Working corectly the output txt

--- main.js
+++ main.js
@@ -4,17 +4,16 @@ import { lexer } from "./lexer.js";
 const filename = "./test.drac";
 const input = String(readFileSync(filename));
 
+
 const tokens = lexer(filename, input);
 
-let result;
+let result = '';
 
 for (const token of tokens) {
-  console.log(token);
+  //console.log(token);
   result += JSON.stringify(token) + "\n";
 }
 
-// Write data in 'Output.txt' .
 writeFile("output.txt", result, (err) => {
-  // In case of a error throw err.
   if (err) throw err;
 });
