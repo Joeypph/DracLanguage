4cbb5d1ef52778fb411d9846dc8f7d0608a79728
ManuelSalvador27 <manuelsalvador2799@gmail.com>
2021-11-13 23:26:59 -0600

    TODO: Finish the lexer, working test 1 and 8

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-import { readFileSync } from "fs"
+import { readFileSync, writeFile } from "fs"
 import { lexer } from "./lexer.js"
 
 const filename = "./test.drac"
 const input = String(readFileSync(filename))
 
 const tokens = lexer(filename, input)
+
+let result
+
 for (const token of tokens) {
-  console.log(token);
+  console.log(token)
+  result += JSON.stringify(token)
 }
+
+// Write data in 'Output.txt' . 
+writeFile('output.txt', result, (err) => { 
+      
+  // In case of a error throw err. 
+  if (err) throw err; 
+}) 
